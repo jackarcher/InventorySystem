85cfadc2024c42c5352c0ec9ac0b745961460711
Jack Archer <jacknimrodarcher@gmail.com>
2015-10-11 03:28:08 +1100

    order doc done

--- src/entity/Order.java
+++ src/entity/Order.java
@@ -1,74 +1,121 @@
 package entity;
 
 import java.io.Serializable;
-import java.util.*;
 
-public class Order implements Serializable{
+/**
+ * Indecate an order in the system
+ * 
+ * @author Archer
+ *
+ */
+public class Order implements Serializable {
+	/**
+	 * The id of the order
+	 */
 	private int id;
+	/**
+	 * The item number in total of the order
+	 */
 	private int itemNumber;
+	/**
+	 * The unallocate number of the order
+	 */
 	private int unallocatedNumber;
+	/**
+	 * The unremoved(or still in stock) number of the order
+	 */
 	private int unremovedNumber;
-	//TODO 
-	HashMap<Integer, ArrayList<Integer>> storageMap;
-	// private boolean orderEstablished;
-	// private boolean orderPayed;
-	// private boolean orderUnderExcution;
-	// private boolean orderFinished;
-	// private boolean orderCancelled;
 
+	/**
+	 * The constructor of this class accept only 2 params
+	 * 
+	 * @param id
+	 *            The order ID.
+	 * @param itemNumber
+	 *            The item number of this order
+	 */
 	public Order(int id, int itemNumber) {
 		this.id = id;
 		this.itemNumber = itemNumber;
 		unallocatedNumber = itemNumber;
 		unremovedNumber = itemNumber;
-		// orderEstablished = true;
-		// orderPayed = false;
-		// orderUnderExcution = false;
-		// orderFinished = false;
-		// orderCancelled = false;
 	}
 
-	// public boolean cancelOrder() {
-	// orderCancelled = true;
-	// return orderCancelled;
-	// }
-
-	public int getId() {
-		return id;
-	}
-
-	public int getItemNumber() {
-		return itemNumber;
-	}
-
-	public int getUnallocatedNumber() {
-		return unallocatedNumber;
-	}
-
-	public void setUnallocatedNumber(int unallocatedNumber) {
-		this.unallocatedNumber = unallocatedNumber;
-	}
-
+	/**
+	 * Exam if the order is allocateable
+	 * 
+	 * @return true for allocateable if unallocate number is not 0, false for
+	 *         unallocateable if unallocate number is 0
+	 */
 	public boolean allocateable() {
 		return (unallocatedNumber == 0 ? false : true);
 	}
 
+	/**
+	 * The getter of the id field
+	 * 
+	 * @return The id of the order
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * The getter of the itemNumber field
+	 * 
+	 * @return The total item number of the order
+	 */
+	public int getItemNumber() {
+		return itemNumber;
+	}
+
+	/**
+	 * The getter of the unallocateNumber field
+	 * 
+	 * @return The unallocate item number of the order
+	 */
+	public int getUnallocatedNumber() {
+		return unallocatedNumber;
+	}
+
+	/**
+	 * The getter of the unremovedNumber field
+	 * 
+	 * @return The unremoved item number of the order
+	 */
+	public int getUnremovedNumber() {
+		return unremovedNumber;
+	}
+
+	/**
+	 * Exam if the order is removeable
+	 * 
+	 * @return true for removeable if unallocate number is 0 and unremoved
+	 *         number is not 0, false for unremoveable
+	 */
 	public boolean removable() {
 		return (unallocatedNumber == 0 && unremovedNumber != 0 ? true : false);
 	}
 
-	// public String getOrderStatus() {
-	// if (orderCancelled)
-	// return "Order cancelled";
-	// // else if (!orderPayed)
-	// // return "Order is waiting payment";
-	// else if (!orderUnderExcution)
-	// return "Order wait excution";
-	// else if (!orderFinished)
-	// return "Order under execution";
-	// else
-	// return "Order finished";
-	// }
+	/**
+	 * The setter of the unallocateNumber
+	 * 
+	 * @param unallocatedNumber
+	 *            the new unallocate number for the order
+	 */
+	public void setUnallocatedNumber(int unallocatedNumber) {
+		this.unallocatedNumber = unallocatedNumber;
+	}
+
+	/**
+	 * The setter of the unremovedNumber
+	 * 
+	 * @param unremovedNumber
+	 *            the new unremoved number for the order
+	 */
+	public void setUnremovedNumber(int unremovedNumber) {
+		this.unremovedNumber = unremovedNumber;
+	}
 
 	@Override
 	public String toString() {
@@ -93,14 +140,6 @@ public class Order implements Serializable{
 		}
 		sb.append("]");
 		return sb.toString();
-	}
-
-	public int getUnremovedNumber() {
-		return unremovedNumber;
-	}
-
-	public void setUnremovedNumber(int unremovedNumber) {
-		this.unremovedNumber = unremovedNumber;
 	}
 
 }
