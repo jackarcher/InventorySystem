b683ed5a09acf27188f356d43c0bc66826215bd7
Jack Archer <jacknimrodarcher@gmail.com>
2015-10-11 12:43:00 +1100

    user doc done
    user doc , sort done

--- src/entity/User.java
+++ src/entity/User.java
@@ -3,26 +3,56 @@ package entity;
 import java.io.Serializable;
 
 /**
- * refers to all users in the system including admins and customers
+ * Refers to all users in the system including administrators and customers
  * 
  * @author Archer
  *
  */
 public abstract class User implements Serializable {
+	/**
+	 * The id of the user
+	 */
 	private String id;
-	private String pwd;
+	/**
+	 * The login flag of the user, being true to refers the login status of the
+	 * user, false for logout
+	 */
 	private boolean loginFlag;
+	/**
+	 * The password of the user
+	 */
+	private String pwd;
 
+	/**
+	 * Initialize a user to contains the given id and password
+	 * 
+	 * @param id
+	 *            The user id
+	 * @param pwd
+	 *            The user password
+	 */
 	public User(String id, String pwd) {
 		this.id = id;
 		this.pwd = pwd;
 		loginFlag = false;
 	}
 
+	/**
+	 * The getter of the id
+	 * 
+	 * @return The ID of the user
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Try to login with the given password
+	 * 
+	 * @param password
+	 *            The password for the user
+	 * @return true if password match the record, false if fail
+	 */
 	public boolean login(String password) {
 		if (pwd.equals(password)) {
 			loginFlag = true;
@@ -32,6 +62,10 @@ public abstract class User implements Serializable {
 		return loginFlag;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean logout() {
 		loginFlag = false;
 		return !loginFlag;
